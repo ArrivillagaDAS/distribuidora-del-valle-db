3661e19a070ef90b914c785bce54f4afcdfdecf2
ArrivillagaDAS <sfaaldana101@gmail.com>
2026-08-20 00:37:20 -0600

    fix: Correccion de funcion fn_calcular_total_con_iva_dinamico

--- database/functions.sql
+++ database/functions.sql
@@ -4,12 +4,17 @@ DELIMITER //
 
 -- 1. fn_calcular_total_con_iva
 -- Calcula el total con IVA del pedido (19%) a partir de la suma de subtotales. (Tomamos en cuenta el hecho de que el IVA puede variar, por lo que se hace dinámico)
-CREATE FUNCTION fn_calcular_total_con_iva_dinamico(p_id_pedido BIGINT, p_porcentaje_iva DECIMAL(4,2))
+CREATE FUNCTION fn_calcular_total_con_iva_dinamico(p_id_pedido BIGINT, p_porcentaje_iva DECIMAL(2,2) UNSIGNED)
 RETURNS DECIMAL(10,2)
 READS SQL DATA
 BEGIN
     DECLARE v_total_sin_iva DECIMAL(10,2);
-    
+
+    IF p_porcentaje_iva < 0 OR p_porcentaje_iva > 1 THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'Error: el porcentaje de IVA debe estar entre 0 y 1 (ej. 0.19 para 19%).';
+    END IF;
+
     SELECT COALESCE(SUM(subtotal), 0)
     INTO v_total_sin_iva
     FROM DETALLE_PEDIDO
